262f614cc75c54a9c099a9a9db8f96b5a4bb3876
Devin Leduc <ixeres@gmail.com>
2017-03-29 22:36:25 -0400

    Entered initial rover values/definitions, working on translating inputs into useable arguments.

---
+++ rover.rb
@@ -0,0 +1,70 @@
+# STRETCH ASSIGNMENT: The Mars Rover!
+
+# Breaking the problem down:
+# Class = Rover
+# Initialize the starting position.
+# Behaviours for the rover:
+# read_instruction: accept an instruction, decide to 'move' or 'turn'.
+# move: affects the position of the rover, updates x OR y coordinates.
+#turn: affects the direction of the rover, turns L OR R.
+#States for the rover:
+# x coord: The current X position of the rover.
+# y coord: The current Y position of the rover.
+# direction: The current facing of the rover (N, S, E, W)
+# copy/paste the map/grid:
+# _ _ _ _ _ _ _ _ _
+# 9|_|_|_|_|_|_|_|_|_|        N
+# 8|_|_|_|_|_|_|_|_|_|      W   E
+# 7|_|_|_|_|_|_|_|_|_|        S
+# 6|_|_|_|_|_|_|_|_|_|
+# 5|_|_|_|_|_|_|_|_|_|
+# 4|_|_|_|_|_|_|_|_|_|
+# 3|_|_|_|_|_|_|_|_|_|
+# 2|_|_|_|_|_|_|_|_|_|
+# 1|_|_|_|_|_|_|_|_|_|
+# 0 1 2 3 4 5 6 7 8 9
+
+# #Copy/paste example input/outputs:
+# Test Input 1:
+# 1 2 N                > our start position and direction
+# LMLMLMLMM            > a series of move and turn instructions.
+#
+#
+# Expected Output:
+# 1 3 N                > rover's new position
+#
+#
+# Test Input 2:
+# 3 3 E
+# MMRMMRMRRM
+#
+# Expected Output:
+# 5 1 E
+
+class Rover
+
+  attr_accessor :x_coord, :y_coord, :direction
+
+  def initialize
+    @x_coord = 1
+    @y_coord = 2
+    @direction = 'N'
+
+  def read_instruction(input)
+    array = input.scan(/\w/)
+    array.each do |moves, turns|
+      (moves = 'M')
+      (turns = 'L', 'R')
+    end
+    end
+  end
+
+  def move
+
+  end
+
+  def turn
+    Array['N', 'E', 'S', 'W']
+
+  end
+end
